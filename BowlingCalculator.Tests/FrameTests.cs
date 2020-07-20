95069f043388d6248ca9f3c6cb6d54e678c0779d
3brucksconsulting <40269338+3brucksconsulting@users.noreply.github.com>
2020-07-20 11:15:47 -0500

    Added GetScore_GivenAllZeros_Returns0 test

--- BowlingCalculator.Tests/FrameTests.cs
+++ BowlingCalculator.Tests/FrameTests.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using Xunit;
+using FluentAssertions;
 
 namespace BowlingCalculator.Tests
 {
@@ -8,6 +10,18 @@ namespace BowlingCalculator.Tests
         public void CreateNewFrame()
         {
             var sut = new Frame();
+        }
+
+        [Fact]
+        public void GetScore_GivenAllZeros_Returns0()
+        {
+            var sut = new Frame();
+
+            Enumerable.Repeat(0, 20).ToList().ForEach(x => sut.AddRoll(x));
+
+            var result = sut.GetScore();
+
+            result.Should().Be(0);
         }
     }
 }
--- BowlingCalculator/Frame.cs
+++ BowlingCalculator/Frame.cs
@@ -1,6 +1,21 @@
-﻿namespace BowlingCalculator
+﻿using System;
+using System.Linq;
+
+namespace BowlingCalculator
 {
     public class Frame
     {
+        private int[] _rolls = new int[21];
+        private int _nextRoll = 0;
+
+        public void AddRoll(int pin)
+        {
+            _rolls[_nextRoll++] = pin;
+        }
+
+        public int GetScore()
+        {
+            return _rolls.Sum();
+        }
     }
 }
