61f15e23d657942df6ee03d405c5b5d92bdbd218
3brucksconsulting <40269338+3brucksconsulting@users.noreply.github.com>
2020-07-20 10:10:10 -0500

    Added basic solution structure

---
+++ BowlingCalculator.Source/BowlingCalculator.Source/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BowlingCalculator.Source
+{
+    public class Class1
+    {
+    }
+}
---
+++ BowlingCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BowlingCalculator.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
